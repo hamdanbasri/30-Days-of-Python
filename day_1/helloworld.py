e03221a7c10413939fe9f490f93217381e91ea7c
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-02-27 13:33:08 +0800

    Initial Commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,70 @@
+#EXERCISE LEVEL 2
+#Q1
+print('Python Version: 3.14.2')
+
+#Q2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+#Q3
+print('Hamdan')
+print('Basri')
+print('Malaysia')
+print('30 Days of Python Challenge')
+
+#Q4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Hamdan','Python','Malaysia']))
+print(type('Hamdan'))
+print(type('Basri'))
+print(type('Malaysia'))
+
+#EXERCISE LEVEL 3
+#int
+87
+
+#float
+15.1
+
+#complex
+#3 - 3k
+
+#string
+'Learning is fun'
+
+#boolean
+True
+False
+
+#list
+[0,1,2,3]
+
+#tuple
+('Earth','Kepler','Hellas Planitia')
+
+#set
+{2,3,4,5}
+
+#dictionary
+{
+'first_name': 'Hamdan',
+'last_name' : 'Basri'
+}
+
+#Euclidian distance
+print(f'Eucilidian Distance of (2,3): {2**2 + 3**2}')
+print(f'Eucilidian Distance of (10,8): {10**2 + 8**2}')
+
+# int to float
+num_int = 10
+print('num_int',num_int)         # 10
+num_float = float(num_int)
+print('num_float:', num_float)
---
+++ day_2/variable.py
@@ -0,0 +1,13 @@
+#Day 2: 30 Days of python programming
+
+first_name = 'Hamdan'
+last_name = 'Basri'
+full_name = 'Hamdan Basri'
+country = 'Malaysia'
+city = 'Kuala Lumpur'
+age = '34'
+year = '2026'
+is_married = True
+is_true = True
+is_light_on = False
+console, workstation = 'ps5', 'lenovo'
