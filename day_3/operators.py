6afd9d5fd1bb342842ff37ee5db175fae0412be6
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2026-02-28 14:08:48 +0800

    Day_3 Question 5

---
+++ day_3/operators.py
@@ -0,0 +1,20 @@
+# Day 3: 30 Days of python programming
+
+age = 34
+height = 168.5
+complex_number = 0
+
+base = input('Enter base: ')
+height = input('Enter height: ')
+base_float = float(base)
+height_float = float(height)
+print(f'The area of the triangle is {0.5 * base_float * height_float}' )
+
+a = input('Enter side a: ')
+b = input('Enter side b: ')
+c = input('Enter side c: ')
+a_int = int(a)
+b_int = int(b)
+c_int = int(c)
+perimter = (a_int + b_int + c_int)
+print(f'The perimeter of the triangle is {perimter}')
